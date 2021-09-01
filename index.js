04910ecb24b6323b03dcab5c03753bf8761c4ad5
RiyaG11 <76156905+RiyaG11@users.noreply.github.com>
2021-09-01 21:42:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.mkdirSync('riya');
+
+// fs.writeFileSync("riya/bio.txt", "my name is riya");
+
+// fs.appendFileSync("riya/bio.txt", "this is task 9");
+
+// const data = fs.readFileSync("riya/bio.txt", "utf-8");
+// console.log(data);
+
+// fs.renameSync("riya/bio.txt", "riya/myBio.txt");
+
+// fs.unlinkSync("riya/myBio.txt");
+
+// fs.rmdirSync("riya");
